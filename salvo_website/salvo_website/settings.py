ac28536ffe3647303bfefcaf8f593a676c025602
thunder <justleviackermann@gmail.com>
2026-06-14 20:14:49 +0530

    updating domain

--- salvo_website/salvo_website/settings.py
+++ salvo_website/salvo_website/settings.py
@@ -201,7 +201,7 @@ CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:8000",
     "http://127.0.0.1:8000",
-    'https://salvoaics-salvowebsite.hf.space',
+    'https://salvoaics-website.hf.space',
     'https://huggingface.co'
     
 ]
